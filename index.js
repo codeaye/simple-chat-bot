bf1b0f5295040fa1580dc2dedc0aa216c20716a7
TheHandyUser <62173713+TheHandyUser@users.noreply.github.com>
2021-01-08 14:16:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 exports.chatbot = async function(message) {
+    if(message.author.bot){return}
     message.channel.startTyping();
     const args = message.content.toLowerCase().replace(/<@(.*?)>/, '').replace(/[^0-9a-z]/gi, '').replace(/\s/, '');
     try{
